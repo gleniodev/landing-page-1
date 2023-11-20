b20f7baf8703881581c65a311b3e11d53b0f208a
gleniodev <gleniodev@gmail.com>
2023-11-20 20:54:04 -0300

    Responsive sectionFive finished

--- src/components/SectionFive.tsx
+++ src/components/SectionFive.tsx
@@ -10,42 +10,41 @@ import Link from "next/link";
 export function SectionFive() {
   return (
     <section
-      className=" w-full bg-img_bg_sec5 bg-no-repeat bg-center bg-cover pt-10  mt-20"
+      className=" w-full bg-img_bg_sec5 bg-no-repeat bg-center bg-cover pt-10 pb-20 mt-20 lg:pt-20 lg:pb-20"
       id="sec-5"
     >
       <Container>
-        <div className="w-full flex flex-1 flex-col items-center justify-center gap-20">
+        <div className="w-full flex flex-1 flex-col items-center justify-center gap-10 lg:gap-20">
           <div className="w-full text-center ">
-            <h2 className="text-[48px] font-bold text-white">
+            <h2 className="text-[34px] text-white lg:text-[48px] font-bold text-center">
               Vamos conversar?
             </h2>
           </div>
           <div className="w-full flex justify-center gap-10">
-            <div className="w-full flex flex-col items-start justify-start gap-10 max-w-[570px]">
-              <h1 className="text-[34px] leading-10 text-white font-semibold">
+            <div className="w-full flex flex-col items-center justify-start gap-10 max-w-[570px] lg:items-start p-6">
+              <h1 className="font-semibold text-center text-white text-[20px] md:text-[32px] md:text-start lg:text-[34px] lg:leading-10 ">
                 Alcance seus objetivos de forma rápida e personalizada!
               </h1>
-              <p className=" text-2xl leading-10 text-white">
+              <p className="text-[20px] leading-10 text-white md:text-2xl text-center md:text-start ">
                 Clique para conversar com um de nossos consultores no{" "}
                 <span className="font-semibold text-green-500">WhatsApp</span> e
                 descubra as melhores soluções para você.
               </p>
-              <p className=" text-2xl leading-10 text-white">
+              <p className="text-[20px] md:text-2xl leading-10 text-white text-center md:text-start">
                 Estamos aqui para tornar suas metas uma realidade.
               </p>
-              <span className=" text-[30px] leading-10 text-white font-semibold"></span>
-              <div className="w-[522px]">
+              <div className="w-[300px] md:w-[522px]">
                 <Link href="https://api.whatsapp.com/send/?phone=5583986201833&text=Oi!+Gostaria+de+fazer+uma+simulação+de+empréstimo+consignado&type=phone_number&app_absent=0">
                   <button className="flex items-center justify-center gap-1 w-full h-16 bg-green-500  rounded-[34px] shadow-lg  hover:bg-green-700  transition-all hover:text-white">
                     <Image src={IconZap} alt="WhatsApp" />
-                    <span className="text-[30px] text-white font-semibold ">
+                    <span className="text-[20px] md:text-[30px] text-white font-semibold ">
                       Solicitar Simulação
                     </span>
                   </button>
                 </Link>
               </div>
             </div>
-            <Image className="relative self-end" src={MenImg} alt={"Homem"} />
+            <Image className="hidden lg:block lg:self-end" src={MenImg} alt={"Homem"} />
           </div>
         </div>
       </Container>
